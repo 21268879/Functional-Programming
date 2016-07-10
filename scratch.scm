64f37c84494dad223a49cd5ab4b8b4154ae2b0ce
Mr Tibor Anderko <21268879@student.uwl.ac.uk>
2016-07-10 15:16:12 +0100

    Code explanations added.

--- scratch.scm
+++ scratch.scm
@@ -4,20 +4,20 @@
 (require srfi/13)
 (require srfi/48)
 
-;;definition for questions calling it "responses"
+;;definition for questions association list calling it "responses"
 (define responses
   '((1 "What type of films do you like?")
     (2 "So you like gore?")
-    (7 "Shall I recommend a gory film for you?")
-    (8 "Shall I recommend a non-gory scary film for you?")))
+    (3 "Shall I recommend a gory film for you?")
+    (4 "Shall I recommend a non-gory scary film for you?")))
 
     
-;;definition for possible answers calling it "decisiontable"
+;;definition for possible answers association list calling it "decisiontable"
 (define decisiontable
   '((1 ((comedy) 20) ((very scary) 2) ((thrillers) 6) ((not animated) 5) ((horror) 2) ((scfi) 4))
     (2 ((some) 8) ((a lot) 7) ((yes) 7) ((no) 8) ((not really) 8))
-    (7 ((yes) gory) ((ok) gory) ((no) 0))
-    (8 ((yes) non-gory) ((ok) gory) ((no) 0))))
+    (3 ((yes) gory) ((ok) gory) ((no) 0))
+    (4 ((yes) non-gory) ((ok) gory) ((no) 0))))
 
 ;;definition for procedure "assq-ref"
 (define (assq-ref assqlist id)
@@ -31,16 +31,16 @@
 (define (get-response id)
   (car (assq-ref responses id)))
 
-;;definition for procedure "get-keywords"
+;;definition for procedure "get-keywords" that takes users answers
 (define (get-keywords id)
   (let ((keys (assq-ref decisiontable id)))
-    (map (lambda (key) (car key)) keys)))
+    (map (λ (key) (car key)) keys)))
 
 ;;definition for procedure "list-of-lengths"
 ;; outputs a list in the form: (0 0 0 2 0 0)
 (define (list-of-lengths keylist tokens)
   (map 
-   (lambda (x)
+   (λ (x)
      (let ((set (lset-intersection eq? tokens x)))
        ;; apply some weighting to the result
        (* (/ (length set) (length x)) (length set))))
@@ -51,7 +51,7 @@
   (let ((n (car (sort list-of-numbers >))))
     (if (zero? n)
       #f
-      (list-index (lambda (x) (eq? x n)) list-of-numbers))))
+      (list-index (λ (x) (eq? x n)) list-of-numbers))))
 
 ;;definition for procedure "lookup"
 (define (lookup id tokens)
@@ -64,9 +64,9 @@
 
 ;;definition for procedure "recommend"
 (define (recommend initial-id)
-  (let loop ((id initial-id))
-    (format #t "~a\n> " (get-response id))
-    (let* ((input (read-line))
+  (let loop ([id initial-id])                   ;
+    (format #t "~a\n> " (get-response id))      ;Print out a response based on the id value 
+    (let* ((input (read-line))                  ;get input from a user
            (string-tokens (string-tokenize input))
            (tokens (map string->symbol string-tokens)))
       (let ((response (lookup id tokens)))
@@ -87,4 +87,4 @@
 	       (loop response)))))))
 
 
-(recommend 1)
+(recommend 2)
